bd102629c821d116051431523d90213831db6449
PiotrMakarewicz <sxpgch@gmail.com>
2021-05-23 19:19:35 +0200

    generator finished

--- generator/generator.py
+++ generator/generator.py
@@ -1,10 +1,11 @@
 from neo4j import GraphDatabase, basic_auth, Result
 from dotenv import load_dotenv
 from faker import Faker
+from datetime import datetime
+
 import os
 import random
 import uuid
-
 
 load_dotenv()
 url = os.getenv("NEO4J_URL")
@@ -29,7 +30,7 @@ def create_fake_user() -> dict:
     user = dict()
 
     user['name'] = fake.name()
-    user['creation_date'] = fake.date_between('-5y')
+    user['creation_date'] = fake.date_time_between('-1y')
     user['description'] = fake.paragraph()
     user['role'] = random.choices(['admin', 'none'], [1, 100])[0]
     user['password_hash'] = uuid.uuid4().hex
@@ -38,10 +39,11 @@ def create_fake_user() -> dict:
     return user
 
 
-def create_fake_post(first_possible_creation_date) -> dict:
+def create_fake_post(first_possible_creation_date: datetime, with_photo: bool) -> dict:
     """
     Returns a dictionary representing a Post node that can be later inserted into the database.
     @param first_possible_creation_date: this is the first possible date of this post. It may be used to create a post for a user registered at a certain day
+    @param with_photo: if true, adds placeholder photo url as photo_address, if false makes photo_address an empty string
     @return: a dictionary with attributes equivalent to the ones of a Post node in the database.
     """
     global fake
@@ -49,13 +51,22 @@ def create_fake_post(first_possible_creation_date) -> dict:
     post = dict()
 
     #post['creation_date'] = fake.date_between(first_possible_creation_date)
-    post['creation_date'] = fake.date_between(first_possible_creation_date)
+    post['creation_date'] = fake.date_time_between(first_possible_creation_date)
     
-    post['update_date'] = random.choice([None, fake.date_between_dates(post['creation_date'], None)])
+    post['update_date'] = random.choice([None, fake.date_time_between_dates(post['creation_date'], None)])
     post['content'] = fake.paragraph() 
-    post['photo_address'] = random.choices(['', 'https://via.placeholder.com/500/02f/a0f.png'], [10, 3])[0]
+    post['photo_address'] = 'https://via.placeholder.com/500/02f/a0f.png' if with_photo else ''
 
     return post
+
+def create_fake_tag() -> dict:
+    """
+    Returns a dictionary representing a Tag node that can be later inserted into the database.
+    @return: a dictionary with attributes equivalent to the ones of a Tag node in the database.
+    """
+    global fake
+    return {'name': fake.word()}
+
 
 
 def add_user(user: dict) -> int:
@@ -76,12 +87,22 @@ def add_post(author_id: int, post: dict) -> int:
     @param post: a dictionary with attributes equivalent to the ones of a Post node in the database
     @return: a single number, the ID of a newly added post
     """
-    result = db.run("CREATE (n:Post {{creation_date: '{}', update_date: '{}', content: '{}', photo_address: '{}'}}) RETURN id(n)"
-        .format(post['creation_date'], post['update_date'], post['content'], post['photo_address']))
+    result = db.run("CREATE (n:Post {{content: '{}', creation_date: '{}', update_date: '{}', photo_address: '{}'}}) RETURN id(n)"
+        .format(post['content'], post['creation_date'], post['update_date'], post['photo_address']))
     post_id = result.data()[0]['id(n)']
     result = db.run("MATCH (u:User), (p:Post) WHERE id(u) = {} AND id(p) = {} CREATE (u)-[r:AUTHOR_OF]->(p)"
         .format(author_id, post_id))
     return post_id
+
+
+def add_tag(tag: dict) -> int:
+    """
+    Adds a new tag to the database
+    @param tag: a dictionary with attributes equivalent to the ones of a Tag node in the database.
+    @return: a single number, the ID of a newly added tag
+    """
+    result = db.run("CREATE (t:Tag {{name: '{}'}}) RETURN id(t)".format(tag['name']))
+    return result.data()[0]['id(t)']
 
 
 def add_observes_between(observer_id: int, observed_id: int) -> None:
@@ -94,21 +115,60 @@ def add_observes_between(observer_id: int, observed_id: int) -> None:
         .format(observer_id, observed_id))
 
 
+def add_likes_between(user_id: int, post_id: int) -> None:
+    """
+    Creates the LIKES relationship between the User node represented by user_id and the Post node represented by post_id
+    @param user_id: ID of a User node already existing in the database
+    @param post_id: ID of a Post node already exisiting in the database
+    """
+    db.run("MATCH (u:User), (p:Post) WHERE id(u) = {} AND id(p) = {} CREATE (u)-[r:LIKES]->(p)"
+        .format(user_id, post_id))
+
+
+def add_dislikes_between(user_id: int, post_id: int) -> None:
+    """
+    Creates the DISLIKES relationship between the User node represented by user_id and the Post node represented by post_id
+    @param user_id: ID of a User node already existing in the database
+    @param post_id: ID of a Post node already existing in the database
+    """
+    db.run("MATCH (u:User), (p:Post) WHERE id(u) = {} AND id(p) = {} CREATE (u)-[r:DISLIKES]->(p)"
+        .format(user_id, post_id))
+
+
+def add_refers_to_between(referring_id: int, referred_id: int) -> None:
+    """
+    Creates the REFERS_TO relationship between the Posts node represented by referring_id and referred_id
+    @param referring_id: ID of a Post node already existing in the database
+    @param referred_id: ID of a Post node already existing in the database
+    """   
+    db.run("MATCH (p:Post), (q:Post) WHERE id(p) = {} AND id(q) = {} CREATE (p)-[r:REFERS_TO]->(q)"
+        .format(referring_id, referred_id))
+
+def add_tagged_as_between(post_id: int, tag_id: int) -> None:
+    """
+    Creates the TAGGED_AS relationship between a Post node and a Tag node
+    @param post_id: ID of a Post node already existing in the database
+    @param tag_id: ID of a Tag node already existing in the database
+    """   
+    db.run("MATCH (p:Post), (t:Tag) WHERE id(p) = {} AND id(t) = {} CREATE (p)-[r:TAGGED_AS]->(t)"
+        .format(post_id, tag_id))
+
+
 db_params = {
-    "users": 30,
+    "users": 50,
     "min user posts": 2,
-    "max user posts": 6,
+    "max user posts": 4,
     "min observed by user": 0,
-    "max observed by user": 6,
+    "max observed by user": 10,
     "min liked posts per user": 0,
-    "max liked posts per user": 6,
+    "max liked posts per user": 10,
     "min disliked posts per user": 0,
-    "max disliked posts per user": 6,
+    "max disliked posts per user": 5,
     "tags": 10,
     "posts with photos freq": 0.3,
     "min tags per post": 0,
     "max tags per post": 5,
-    "referring post chance": 0.5
+    "referring post chance": 0.2
 }
 
 
@@ -139,14 +199,62 @@ def generate_database():
     
     for user_id, user in users.items():
         num_posts = random.randint(db_params['min user posts'], db_params['max user posts'])
-        
         for _ in range(num_posts):
-            add_post(user_id, create_fake_post(user['creation_date']))
+            post = create_fake_post(user['creation_date'], with_photo=(random.uniform(0, 1) > db_params['posts with photos freq']))
+            post_id = add_post(user_id, post)
+            posts[post_id] = post
+    
+    ### Add LIKES and DISLIKES relationships
+
+    for user_id, user in users.items():
+        num_liked_posts = random.randint(db_params['min liked posts per user'], db_params['max liked posts per user'])
+        num_disliked_posts = random.randint(db_params['min disliked posts per user'], db_params['max disliked posts per user'])
+        liked_posts_ids = random.sample(posts.keys(), k=num_liked_posts)
+        disliked_posts_ids = random.sample(set(posts.keys()).difference(set(liked_posts_ids)), k=num_disliked_posts)
+        for post_id in liked_posts_ids:
+            add_likes_between(user_id, post_id)
+        for post_id in disliked_posts_ids:
+            add_dislikes_between(user_id, post_id)
+
+    ### Add REFERS_TO relationships
+
+    def get_random_post_created_before(datetime: datetime):
+        nonlocal posts
+
+        for _ in range(25):
+            post_id, post = random.choice(list(posts.items()))
+            if post['creation_date'] < datetime:
+                return post_id
+        
+        return None
 
 
+    for referring_id, referring_post in posts.items():
+        if random.uniform(0, 1) < db_params['referring post chance']:
+            while True:
+                referred_id = get_random_post_created_before(referring_post['creation_date'])
+                if referred_id != referring_id:
+                    break
+            if referred_id is not None:
+                add_refers_to_between(referring_id, referred_id)
+    
+    ### Add Tag nodes
+
+    for _ in range(db_params['tags']):
+        tag = create_fake_tag()
+        tag_id = add_tag(tag)
+        tags[tag_id] = tag 
+    
+    ### Add TAGGED_AS relationships
+    
+    for post_id, post in posts.items():
+        num_tags = random.randint(db_params["min tags per post"], db_params["max tags per post"])
+        tag_ids = random.sample(tags.keys(), k=num_tags)
+        for tag_id in tag_ids:
+            add_tagged_as_between(post_id, tag_id)
 
 
-
-generate_database()
-
-db.close()
+try:
+    generate_database()
+finally:
+    db.close()
