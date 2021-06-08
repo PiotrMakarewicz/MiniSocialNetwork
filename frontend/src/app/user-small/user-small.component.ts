fa7ded174f69fb34163fc01c6d962b439af02158
Gabriel Kępka <gkepka@student.agh.edu.pl>
2021-06-08 23:28:57 +0200

    user-small wstępny commit

--- frontend/src/app/user-small/user-small.component.ts
+++ frontend/src/app/user-small/user-small.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { backendAddress } from '../global-variables';
 
 @Component({
   selector: 'app-user-small',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSmallComponent implements OnInit {
 
+  @Input() id: any;
+  private user: any;
+  name: any;
+  imageUrl: any;
+  address: any;
+
+
   constructor() { }
 
+  async getUser() {
+    if (this.id) {
+      const result = await fetch(backendAddress + this.id);
+      const json = await result.json();
+      this.user = json['users'][0];
+      this.name = this.user['name'];
+      this.imageUrl = this.user['imageUrl'];
+      this.address = backendAddress + this.id;
+      return json['users'][0]
+    }
+  }
+
   ngOnInit() {
+    this.getUser();
   }
 
 }
