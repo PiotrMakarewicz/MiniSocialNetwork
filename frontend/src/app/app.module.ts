e3fa913d5377ec88cf56303344e820380cb915db
PiotrMakarewicz <sxpgch@gmail.com>
2021-06-08 14:18:19 +0200

    login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { UserSmallComponent } from './user-small/user-small.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    PostComponent,
+    PostListComponent,
+    UserSmallComponent,
+    UserListComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
