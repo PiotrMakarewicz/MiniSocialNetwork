01dd09e8cbdeb4cacc610b2343f83b5e280ab920
PiotrMakarewicz <sxpgch@gmail.com>
2021-06-08 21:04:48 +0200

    update

--- frontend/src/app/login.service.ts
+++ frontend/src/app/login.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
+import { MD5 } from './md5'
+import { backendAddress} from './global-variables'
+import { Router } from '@angular/router';
 
 @Injectable()
 export class LoginService {
 
-  constructor() { }
+  loggedIn: boolean = false;
+  userName: string = "";
+  userId: any = null;
+  passwordHash: string = "";
 
+  constructor(private router: Router) { }
+
+  async loginAs(userName: string, password: string){
+    console.log(userName, MD5(password));
+    let response = await fetch(backendAddress + 'password_check/' + userName + '/' + MD5(password))
+    let json = await response.json();
+    console.log(json);
+    if (json['id']){
+      this.loggedIn = true;
+      this.userName = userName;
+      this.userId = json['id'];
+      this.passwordHash = MD5(password);
+      alert("Logged in as " + this.userName + ".");
+      return true;
+    }
+    else{
+      alert("Failed to sign in.");
+      return false;
+    }
+  }
+  
+
+  isLoggedIn() {
+
+    return this.loggedIn;
+  }
 }
