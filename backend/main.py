191a21d31986febb1f3bb21571e4103c3b521b24
Gabriel Kępka <gkepka@student.agh.edu.pl>
2021-06-08 15:20:32 +0200

    dodanie id do zapytan

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,8 @@ def get_all_users():
         "u.creation_datetime as creation_datetime, "
         "u.avatar as avatar, "
         "u.description as description, "
-        "u.role as role"
+        "u.role as role, "
+        "id(u) as id"
         "")))
     users = []
     for user in results:
@@ -45,46 +46,49 @@ def get_all_users():
         'avatar': user['avatar'],
         'description': user['description'],
         'role': user['role'],
+        'id': user['id'],
     })
     return json.dumps({"users": users})
 
 
-@app.route("/<username>")
-def get_user(username):
+@app.route("/<userID>")
+def get_user(userID):
     db = get_db()
     results = db.read_transaction(lambda tx: list(tx.run(
-        "Match (u:User)"
-        "WHERE u.name = $username "
+        "Match (u:User {id: $userID})"
         "RETURN u.name as name, "
         "u.creation_datetime as creation_datetime, "
         "u.avatar as avatar, "
         "u.description as description, "
-        "u.role as role"
-        "", {'username': username})))
-    
+        "u.role as role, "
+        "id(u) as id"
+        "", {'userID': userID})))
+    users = []
     for result in results:
-        user = {
+        users.append({
             'name': result['name'],
             'creation_datetime': result['creation_datetime'],
             'avatar': result['avatar'],
             'description': result['description'],
             'role': result['role'],
-        }   
-    return json.dumps({"user": user})
+            'id': result['id'],
+        })
+    return json.dumps({"users": users})
 
 
-@app.route("/<username>/observed")
-def get_observed_by_user(username):
+@app.route("/<userID>/observed")
+def get_observed_by_user(userID):
     db = get_db()
     results = db.read_transaction(lambda tx: list(tx.run(
         "Match (u:User)-[o:OBSERVES]->(b:User) "
-        "WHERE u.name = $username "
+        "WHERE id(u) = $userID "
         "RETURN b.name as name, "
         "b.creation_datetime as creation_datetime, "
         "b.avatar as avatar, "
         "b.description as description, "
-        "b.role as role"
-        "", {'username': username})))
+        "b.role as role, "
+        "id(b) as id"
+        "", {'userID': userID})))
     observed = []
     for user in results:
         observed.append({
@@ -93,22 +97,24 @@ def get_observed_by_user(username):
         'avatar': user['avatar'],
         'description': user['description'],
         'role': user['role'],
+        'id': user['id'],
     })
     return json.dumps({"observed": observed})
 
 
-@app.route("/<username>/observing")
-def get_observing_user(username):
+@app.route("/<userID>/observing")
+def get_observing_user(userID):
     db = get_db()
     results = db.read_transaction(lambda tx: list(tx.run(
         "Match (u:User)<-[o:OBSERVES]-(b:User) "
-        "WHERE u.name = $username "
+        "WHERE id(u) = $userID "
         "RETURN b.name as name, "
         "b.creation_datetime as creation_datetime, "
         "b.avatar as avatar, "
         "b.description as description, "
-        "b.role as role"
-        "", {'username': username})))
+        "b.role as role, "
+        "id(b) as id"
+        "", {'userID': userID})))
     observing = []
     for user in results:
         observing.append({
@@ -117,46 +123,50 @@ def get_observing_user(username):
         'avatar': user['avatar'],
         'description': user['description'],
         'role': user['role'],
+        'id': user['id'],
     })
     return json.dumps({"observing": observing})
 
 
-@app.route("/<username>/posts")
-def get_posts_by_user(username):
+@app.route("/<userID>/posts")
+def get_posts_by_user(userID):
     db = get_db()
     results = db.read_transaction(lambda tx: list(tx.run(
         "Match (u:User)-[a:AUTHOR_OF]->(p:Post) "
-        "WHERE u.name = $username "
+        "WHERE id(u) = $userID "
         "RETURN p.creation_datetime as creation_datetime, "
         "p.photo_address as photo_address, "
         "p.content as content, "
-        "p.update_datetime as update_datetime"
-        "", {'username': username})))
+        "p.update_datetime as update_datetime, "
+        "id(p) as id"
+        "", {'userID': userID})))
     posts = []
     for post in results:
         posts.append({
-        'author': username,
+        'author': userID,
         'creation_datetime': post['creation_datetime'],
         'photo_address': post['photo_address'],
         'content': post['content'],
-        'update_datetime': post['update_datetime']
+        'update_datetime': post['update_datetime'],
+        'id': post['id'],
         })
     return json.dumps({"posts": posts})
 
 
-@app.route("/<username>/liked")
-def get_liked_by_user(username):
+@app.route("/<userID>/liked")
+def get_liked_by_user(userID):
     db = get_db()
     results = db.read_transaction(lambda tx: list(tx.run(
         "Match (u:User)-[l:LIKES]->(p:Post) "
         "Match (c:User)-[a:AUTHOR_OF]->(p) "
-        "where u.name = $username "
+        "where id(u) = $userID "
         "RETURN p.creation_datetime as creation_datetime, "
         "p.photo_address as photo_address, "
         "p.content as content, "
         "p.update_datetime as update_datetime, "
-        "c.name as author"
-        "", {'username': username})))
+        "c.name as author, "
+        "id(p) as id"
+        "", {'userID': userID})))
     posts = []
     for post in results:
         posts.append({
@@ -164,24 +174,26 @@ def get_liked_by_user(username):
         'creation_datetime': post['creation_datetime'],
         'photo_address': post['photo_address'],
         'content': post['content'],
-        'update_datetime': post['update_datetime']
+        'update_datetime': post['update_datetime'],
+        'id': post['id'],
         })
     return json.dumps({"posts": posts})
 
 
-@app.route("/<username>/disliked")
-def get_disliked_by_user(username):
+@app.route("/<userID>/disliked")
+def get_disliked_by_user(userID):
     db = get_db()
     results = db.read_transaction(lambda tx: list(tx.run(
         "Match (u:User)-[l:DISLIKES]->(p:Post) "
         "Match (c:User)-[a:AUTHOR_OF]->(p) "
-        "where u.name = $username "
+        "where id(u) = $userID "
         "RETURN p.creation_datetime as creation_datetime, "
         "p.photo_address as photo_address, "
         "p.content as content, "
         "p.update_datetime as update_datetime, "
-        "c.name as author"
-        "", {'username': username})))
+        "c.name as author, "
+        "id(p) as id"
+        "", {'userID': userID})))
     posts = []
     for post in results:
         posts.append({
@@ -189,24 +201,26 @@ def get_disliked_by_user(username):
         'creation_datetime': post['creation_datetime'],
         'photo_address': post['photo_address'],
         'content': post['content'],
-        'update_datetime': post['update_datetime']
+        'update_datetime': post['update_datetime'],
+        'id': post['id'],
         })
     return json.dumps({"posts": posts})
 
 
-@app.route("/<username>/observed/posts")
-def get_posts_by_observed(username):
+@app.route("/<userID>/observed/posts")
+def get_posts_by_observed(userID):
     db = get_db()
     results = db.read_transaction(lambda tx: list(tx.run(
         "Match (u:User)-[o:OBSERVES]->(b:User) "
         "Match (b)-[a:AUTHOR_OF]->(p:Post) "
-        "where u.name = $username "
+        "where id(u) = $userID "
         "RETURN p.creation_datetime as creation_datetime, "
         "p.photo_address as photo_address, "
         "p.content as content, "
         "p.update_datetime as update_datetime, "
         "b.name as author"
-        "", {'username': username})))
+        "id(p) as id"
+        "", {'userID': userID})))
     posts = []
     for post in results:
         posts.append({
@@ -214,9 +228,20 @@ def get_posts_by_observed(username):
         'creation_datetime': post['creation_datetime'],
         'photo_address': post['photo_address'],
         'content': post['content'],
-        'update_datetime': post['update_datetime']
+        'update_datetime': post['update_datetime'],
+        'id': post['id'],
         })
     return json.dumps({"posts": posts})
+
+# @app.route("/<userID>/like/<postID>")
+# def like_post(userID, postID):
+#     db = get_db()
+#     results = db.read_transaction(lambda tx: list(tx.run(
+#         "Match (u:User) "
+#         "Match (p:Post) "
+#         "where id(u) = $userID AND id(p) = $postID "
+#         "CREATE (u)-[l:LIKES {datetime: datetime()}]->(p)"
+#         "", {'userID': userID})))
 
 
 if __name__ == '__main__':
