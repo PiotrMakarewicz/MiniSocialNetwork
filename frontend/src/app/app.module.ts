5a7fa3c7198b8000525e42a8fffae66d7c5a726b
PiotrMakarewicz <sxpgch@gmail.com>
2021-06-08 13:54:38 +0200

    dodano bootstrapa

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,31 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
-import { UserPageComponent } from './user-page/user-page.component';
-import { UserSmallComponent } from './user-small/user-small.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { PostListComponent } from './post-list/post-list.component';
-import { UserListComponent } from './user-list/user-list.component';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostComponent,
-    UserPageComponent,
-    UserSmallComponent,
-    LoginPageComponent,
-    PostListComponent,
-    UserListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
