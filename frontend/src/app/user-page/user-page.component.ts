0cfb307f77b5bccc9111855eb1a98a7196ddaacc
Gabriel Kępka <gkepka@student.agh.edu.pl>
2021-06-09 00:48:03 +0200

    obserwowanie

--- frontend/src/app/user-page/user-page.component.ts
+++ frontend/src/app/user-page/user-page.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnDestroy, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
+import { backendAddress } from '../global-variables';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.css']
+  styleUrls: ['./user-page.component.css'],
+  providers: [ UserService]
 })
-export class UserPageComponent implements OnInit {
+export class UserPageComponent implements OnInit, OnDestroy {
   @Output() id: any;
   private sub: any;
   @Output() imageUrl: any;
   @Output() name: any;
   @Output() description: any;
   @Output() user: any;
+  observeLink: any;
+  unobserveLink: any;
 
-  constructor(private route: ActivatedRoute, private userService: UserService) { }
+  constructor(private route: ActivatedRoute, private userService: UserService, private loginService: LoginService) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(async params => {
@@ -25,12 +30,25 @@ export class UserPageComponent implements OnInit {
        this.imageUrl = this.user['avatar'];
        this.description = this.user['description'];
        this.name = this.user['name'];
+       this.observeLink = backendAddress + this.loginService.getUserId() + "/observe/" + this.id;
+       this.unobserveLink = backendAddress + this.loginService.getUserId() + "/observe/" + this.id;
     });
   }
 
   ngOnDestroy() {
-    if (this.sub)
-      this.sub.unsubscribe();
+    this.sub.unsubscribe();
+  }
+
+  async observe() {
+    let result = await fetch(this.observeLink);
+
+    console.log('observe')
+    //await this.update();
+  }
+  async unobserve() {
+    let result = await fetch(this.unobserveLink);
+    console.log('unobserve')
+    //await this.update();
   }
 
 }
